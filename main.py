baf01953eb3919af20bc5505997998acaa0cdd9d
arturoruge <arturoruge@gmail.com>
2017-11-10 09:47:41 -0300

    Ajuste: usuario no puede hacer consultas sin estar logueado

--- main.py
+++ main.py
@@ -51,13 +51,16 @@ def logout():
 @app.route("/ventas")
 def ventas():
 	session = functions.userInSession()
-	login = functions.messageUserSession(session)
+	if session: 
+		login = functions.messageUserSession(session)
 
-	dataSales = functions.readSales()
-	countSales = len(dataSales)
+		dataSales = functions.readSales()
+		countSales = len(dataSales)
 
-	title = "Consulta las ventas del mes"
-	return render_template("ventas.html", title=title, dataSales = dataSales, countSales = countSales , login = login)
+		title = "Consulta las ventas del mes"
+		return render_template("ventas.html", title=title, dataSales = dataSales, countSales = countSales , login = login)
+	else:
+		return redirect(url_for("index"))
 
 
 @app.route("/productosporcliente", methods=["GET", "POST"])
@@ -67,22 +70,24 @@ def productosPorClientes(client=None):
 	positionClient = None
 	listClients = None
 	session = functions.userInSession()
-	login = functions.messageUserSession(session)
+	if session:
+		login = functions.messageUserSession(session)
 
-	dataSales = functions.readSales()
-	client_form = forms.ClientsForm(request.form)
+		dataSales = functions.readSales()
+		client_form = forms.ClientsForm(request.form)
 
-	if request.method == "POST" and client_form.validate():
-		clientSearchOriginal = client_form.client.data
-		clientSearch = clientSearchOriginal.upper()
-		positionClient = functions.positionData("CLIENTE")
-		listClients = functions.searchData(clientSearch, dataSales, positionClient)
-		if len(listClients) == 0:
-			listClients = None
-		
-	title = "Consulta las compras realizadas por cliente"
-	return render_template("productos_por_cliente.html", client = client, form = client_form, title=title, login = login, dataSales = dataSales, option = clientSearch, listClients = listClients)
-
+		if request.method == "POST" and client_form.validate():
+			clientSearchOriginal = client_form.client.data
+			clientSearch = clientSearchOriginal.upper()
+			positionClient = functions.positionData("CLIENTE")
+			listClients = functions.searchData(clientSearch, dataSales, positionClient)
+			if len(listClients) == 0:
+				listClients = None
+			
+		title = "Consulta las compras realizadas por cliente"
+		return render_template("productos_por_cliente.html", client = client, form = client_form, title=title, login = login, dataSales = dataSales, option = clientSearch, listClients = listClients)
+	else:
+		return redirect(url_for("index"))
 
 
 @app.route("/clientesporproducto", methods=["GET", "POST"])
@@ -92,65 +97,71 @@ def clientesPorProducto(product=None):
 	positionProduct = None
 	listProducts = None
 	session = functions.userInSession()
-	login = functions.messageUserSession(session)
+	if session:
+		login = functions.messageUserSession(session)
 
-	dataSales = functions.readSales()
-	product_form = forms.ProductForm(request.form)
+		dataSales = functions.readSales()
+		product_form = forms.ProductForm(request.form)
 
-	if request.method == "POST" and product_form.validate():
-		productSearchOriginal = product_form.product.data
-		productSearch = productSearchOriginal.upper()
-		positionProduct = functions.positionData("PRODUCTO")
-		listProducts = functions.searchData(productSearch, dataSales, positionProduct)
-		if len(listProducts) == 0:
-			listProducts = None
-		
-	title = "Consulta los clientes que compraron un producto"
-	return render_template("clientes_por_producto.html", product = product, form = product_form, title=title, login = login, dataSales = dataSales, option = productSearch, listProducts = listProducts)
-
+		if request.method == "POST" and product_form.validate():
+			productSearchOriginal = product_form.product.data
+			productSearch = productSearchOriginal.upper()
+			positionProduct = functions.positionData("PRODUCTO")
+			listProducts = functions.searchData(productSearch, dataSales, positionProduct)
+			if len(listProducts) == 0:
+				listProducts = None
+			
+		title = "Consulta los clientes que compraron un producto"
+		return render_template("clientes_por_producto.html", product = product, form = product_form, title=title, login = login, dataSales = dataSales, option = productSearch, listProducts = listProducts)
+	else:
+		return redirect(url_for("index"))
 
 @app.route("/productosmasvendidos", methods = ["GET", "POST"])
 def productosMasVendidos():
 
 	session = functions.userInSession()
-	login = functions.messageUserSession(session)
-	numberItems = None
-	
-	numberForm = forms.numberForm(request.form)
-	if request.method == "POST" and numberForm.validate():
-		numberItems = int(numberForm.numberItems.data)
+	if session:
+		login = functions.messageUserSession(session)
+		numberItems = None
+		
+		numberForm = forms.numberForm(request.form)
+		if request.method == "POST" and numberForm.validate():
+			numberItems = int(numberForm.numberItems.data)
 
-	dataSales = functions.readSales()
-	positionProduct = functions.positionData("PRODUCTO")
-	positionAmount = functions.positionData("CANTIDAD")
-	positionCode = functions.positionData("CODIGO")
-	topProducts = functions.orderProducts(dataSales, positionProduct, positionAmount, positionCode)
+		dataSales = functions.readSales()
+		positionProduct = functions.positionData("PRODUCTO")
+		positionAmount = functions.positionData("CANTIDAD")
+		positionCode = functions.positionData("CODIGO")
+		topProducts = functions.orderProducts(dataSales, positionProduct, positionAmount, positionCode)
 
-	title= "Los productos mas vendidos este mes"
-	return render_template("productosmasvendidos.html", title = title, login = login, topProducts = topProducts, numberItems = numberItems, form = numberForm)
-
+		title= "Los productos mas vendidos este mes"
+		return render_template("productosmasvendidos.html", title = title, login = login, topProducts = topProducts, numberItems = numberItems, form = numberForm)
+	else:
+		return redirect(url_for("index"))
 
 @app.route("/clientesmascompradores", methods = ["GET", "POST"])
 def clientesmascompradores():
 
 	session = functions.userInSession()
-	login = functions.messageUserSession(session)
-	numberItems = None
+	if session:
+		login = functions.messageUserSession(session)
+		numberItems = None
 
-	numberForm = forms.numberForm(request.form)
-	if request.method == "POST" and numberForm.validate():
-		if numberForm.numberItems.data:
-			numberItems = int(numberForm.numberItems.data)
+		numberForm = forms.numberForm(request.form)
+		if request.method == "POST" and numberForm.validate():
+			if numberForm.numberItems.data:
+				numberItems = int(numberForm.numberItems.data)
 
-	dataSales = functions.readSales()
-	positionClient = functions.positionData("CLIENTE")
-	positionPrice = functions.positionData("PRECIO")
-	topClients = functions.orderClients(dataSales, positionClient, positionPrice)
+		dataSales = functions.readSales()
+		positionClient = functions.positionData("CLIENTE")
+		positionPrice = functions.positionData("PRECIO")
+		topClients = functions.orderClients(dataSales, positionClient, positionPrice)
 
 
-	title= "Clientes mas compradores de este mes"
-	return render_template("clientesmascompradores.html", title = title, login = login, topClients = topClients, numberItems = numberItems, form = numberForm)
-
+		title= "Clientes mas compradores de este mes"
+		return render_template("clientesmascompradores.html", title = title, login = login, topClients = topClients, numberItems = numberItems, form = numberForm)
+	else:
+		return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
